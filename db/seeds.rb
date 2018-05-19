476ae1491ffcba8e20241d52dcce17e9a34bda8f
Lalit Kumar Maurya <lalit.maurya@engineer.ai>
2018-05-19 23:05:24 +0530

    Imrovements

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+MobilePhone.delete_all
+
+brand = ["Samsung", "Panasonic", "Micromax", "Nokia", "Sony", "Lava", "Adcom", "Blackbear", "BlackBear", "BlackZone", "Blackzone", "BQ", "BSNL", "Callbar", "Celkon", "Champion", "Darago", "Datawind", "Forme", "GAMMA", "Gfive", "Gionee", "Good One", "Haier", "Heemax", "HPL", "HTC", "Huawei", "Iball", "Intex", "Ismart", "Itel", "iVooMi", "Jinga", "Jio", "JIVI", "Kara", "Karbonn", "Kechaoda", "Kimfly", "Lemon", "Lenovo", "Lychee", "LYCHEE", "M-Horse", "MAXX", "Mtech", "nuvo", "Onida", "Peace", "Philips", "RAGE", "Reach", "Reliance", "Rio", "Sansui", "Spice", "Subway", "Swipe", "T-Max", "Tecoze", "Trio", "TYMES", "UI Phones", "Videocon", "Vox", "Wham", "Whitecherry", "XCCESS", "Xolo", "Yxtel", "Zen", "Ziox", "ZOPO", "ZTE", "Acer", "AIEK", "AIRI Mobile", "AK", "Alcatel", "Animate", "Apple", "Asus", "Billion", "Binatone", "Blackberry", "BLU", "Callmate", "Camerii", "CAT", "Cheers", "Colors", "Comio", "Coolpad", "Diamond", "Doel", "Fox", "Gigaset", "Google", "GS", "Hi Tech", "Honor", "HP", "Infinix", "InFocus", "Kenxinda", "Kodak", "LG", "LYF", "Mi", "Microsoft", "Mido", "MOBONE", "Motorola", "Nubia", "Okwu", "OnePlus", "OPPO", "Smartron", "TCL", "Tecno", "TP-Link", "Uniscope", "VIVO", "Voicair", "Voto", "XOLO", "Yu"]
+
 (1..5000).each do |i|
-  brand = [:samsung, :apple, :huwai, :mi, :oneplus, :oppo, :vivo, :motorola, :jbl, :boat, :honor, :asus].sample
+  sample_brand = brand.sample
   MobilePhone.create!(
-    name: "#{brand.to_s.humanize} #{Faker::App.name}",
+    name: "#{sample_brand} #{Faker::App.name}",
     description: Faker::Lorem.paragraphs(4).join(' '),
-    brand: brand,
+    brand: sample_brand,
     price: Faker::Number.between(6000, 85000),
     ram: Faker::Number.between(1, 8),
     screen_size: Faker::Number.between(1, 6) * 1.25,
@@ -22,3 +26,5 @@
     avatar: Faker::Avatar.image
   )
 end
+
+MobilePhone.reindex
