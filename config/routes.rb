e5bcee1619e88734860564531156168499c2c099
Lalit Kumar Maurya <lalit.maurya@engineer.ai>
2018-05-19 12:10:51 +0530

    Postgresql with Rails Admin

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
